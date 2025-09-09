1d7fb3f613fd7a7294f94f04cb2633e2da52ebda
ypatrick21 <ypatrickflor@gmail.com>
2025-09-09 10:51:10 +0800

    added inertia js to tech stack Homeventory

--- src/data/projects.js
+++ src/data/projects.js
@@ -32,7 +32,7 @@ const projects = [
         title: "Homeventory WebApp",
         description: "A personal project web application designed to help users manage and organize their home inventory. It provides a user-friendly interface for adding, editing, and categorizing household items, making it easy to keep track of belongings.",
         image: "/images/projects/homeventory.png",
-        techStack: ["React + TS Vite", "Tailwind CSS", "Shadcn UI", "Laravel", "SQLite3" ],
+        techStack: ["React + TS Vite", "Tailwind CSS", "Shadcn UI", "Laravel", "SQLite", "Inertia.js" ],
     },
     {
         id: "mynote-webapp",
